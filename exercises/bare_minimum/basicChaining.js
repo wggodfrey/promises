4061342a0b7bbe6b12ca2968d65437a1b45f3dd0
wggodfrey <w.gowman.godfrey@gmail.com>
2018-10-09 14:23:52 -0700

    Pass all tests in basicChaining except last test

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -8,12 +8,35 @@
  * HINT: We exported some similar promise-returning functions in previous exercises
  */
 
+// var testRoll = function(param1, param2, callback) {
+//   fs.readFile('./rolltest.txt', 'utf8', (err, data) => {
+//     if (err) {
+//       callback(err);
+//     } else {
+//       callback(null, param1, param2);
+//     }
+//   });
+// }
+
+// testRoll(5,6, (param1, param2) => {
+//   console.log(param1, param2)
+// })
+
 var fs = require('fs');
 var Promise = require('bluebird');
-
-
+var pluckFirstLineAsync = require('./promiseConstructor').pluckFirstLineFromFileAsync;
+var getGitHubProfileAsync = require('./promisification').getGitHubProfileAsync; 
+var writeFileAsync = Promise.promisify(fs.writeFile);
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
+  return pluckFirstLineAsync(readFilePath)
+    .then(getGitHubProfileAsync)
+    .then((response) => {
+      // console.log(response);
+      // writeFileAsync(writeFilePath);
+      
+    })
+    .catch((e) => { console.log(e); });
   // TODO
 };
 
