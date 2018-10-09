7b70b3ddba5b33f3c79aa792e2ff6903bec432d7
wggodfrey <w.gowman.godfrey@gmail.com>
2018-10-08 19:59:28 -0700

    Solved callback review problems

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,12 +6,33 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, callback) {
+  var body = '';
+  var stream = fs.createReadStream(filePath, {encoding: 'utf8'});
+  stream.on('data', (chunk) => {
+    body += chunk;
+    if (chunk.indexOf('\n') !== -1) {
+      stream.close();
+    }
+  });
+  stream.on('error', (err) => {
+    callback(err, null);
+  });
+  stream.on('close', () => {
+    callback(null, body.slice(0, body.indexOf('\n')));
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
+var getStatusCode = function (url, callback) {
+  request(url, (err, res, body) => {
+    if (err) {
+      callback(new Error('Invalid URI'));
+    } else {
+      callback(err, res.statusCode);
+    }
+    // console.log('OUR STATUS CODE:', res.);
+  });
   // TODO
 };
 
